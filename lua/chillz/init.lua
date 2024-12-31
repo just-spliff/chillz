e262f1f85e8a68cadf349b2980b20edf1c454dbc
Maciej Gierałtowski <mgieraltowski02@gmail.com>
2024-12-31 19:12:36 +0100

    .

--- lua/chillz/init.lua
+++ lua/chillz/init.lua
@@ -1,14 +1,19 @@
 local M = {}
-local config = require("chillz.config")
-local highlights = require("chillz.highlights")
-local util = require("chillz.util")
 
 M.setup = function(opts)
-	config.setup(opts)
-	local groups = highlights.setup()
-	for group, props in pairs(groups) do
-		util.highlight(group, props)
+	opts = opts or {}
+
+	-- Wczytaj kolory i grupy highlight
+	local colors = require("chillz.colors").palette
+	local highlights = require("chillz.highlights").setup(colors)
+
+	-- Zastosuj highlighty
+	for group, settings in pairs(highlights) do
+		vim.api.nvim_set_hl(0, group, settings)
 	end
+
+	-- Zastosuj opcje konfiguracyjne (opcjonalnie)
+	require("chillz.config").apply(opts)
 end
 
 return M
