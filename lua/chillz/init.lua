09a1d1d2318c419c4d8829804482bccb8a1206a7
Maciej Gierałtowski <mgieraltowski02@gmail.com>
2024-12-31 14:45:15 +0100

    add more colors and make it more contrast

--- lua/chillz/init.lua
+++ lua/chillz/init.lua
@@ -1,32 +1,74 @@
 local M = {}
 
--- Definicja palety kolorów
+-- Paleta kolorów
 M.colors = {
-	bg = "#0f1e1e",
-	fg = "#e0f0e0",
-	mint = "#a8d5ba",
-	accent = "#74c8a2",
-	gray = "#4a5d5d",
-	dark_mint = "#4e7f68",
+	-- Tło i tekst
+	bg = "#0d1a1a", -- Ciemne tło
+	bg_alt = "#102626", -- Nieco jaśniejsze tło
+	fg = "#d8f8f8", -- Główny kolor tekstu
+	fg_dim = "#a0c4c4", -- Przygaszony tekst
+
+	-- Miętowe akcenty
+	mint_light = "#a8ffde",
+	mint = "#74d6b8",
+	mint_dark = "#3c8d77",
+
+	-- Kontrastowe akcenty
+	orange = "#ffb86c",
+	red = "#ff6e6e",
+	yellow = "#f3f99d",
+	blue = "#62d6e8",
+	purple = "#c792ea",
+	pink = "#ff92df",
+
+	-- Dodatkowe kolory
+	gray = "#546a6a",
+	highlight = "#1f3636",
+	error = "#ff5555",
+	warning = "#f1fa8c",
+	info = "#8be9fd",
+	success = "#50fa7b",
 }
 
--- Funkcja do ustawiania kolorów
+-- Funkcja do ustawiania grup highlight
 local function set_highlight(group, opts)
 	vim.api.nvim_set_hl(0, group, opts)
 end
 
--- Główna funkcja ładująca schemat
+-- Funkcja do ładowania schematu
 function M.load()
 	local colors = M.colors
 
-	-- Przykładowe grupy highlight
+	-- Ustawienia podstawowe
 	set_highlight("Normal", { fg = colors.fg, bg = colors.bg })
-	set_highlight("Comment", { fg = colors.gray, italic = true })
-	set_highlight("Constant", { fg = colors.mint })
-	set_highlight("Function", { fg = colors.accent, bold = true })
-	set_highlight("Statement", { fg = colors.dark_mint, italic = true })
+	set_highlight("NormalNC", { fg = colors.fg_dim, bg = colors.bg_alt })
+	set_highlight("Comment", { fg = colors.fg_dim, italic = true })
+	set_highlight("Constant", { fg = colors.orange })
+	set_highlight("Identifier", { fg = colors.mint_light })
+	set_highlight("Statement", { fg = colors.blue })
+	set_highlight("PreProc", { fg = colors.purple })
+	set_highlight("Type", { fg = colors.yellow })
 
-	-- Możesz dodać więcej grup...
+	-- Ustawienia dla błędów i ostrzeżeń
+	set_highlight("Error", { fg = colors.error, bold = true })
+	set_highlight("WarningMsg", { fg = colors.warning, bold = true })
+	set_highlight("Info", { fg = colors.info })
+	set_highlight("Success", { fg = colors.success, bold = true })
+
+	-- Podświetlenia
+	set_highlight("Visual", { bg = colors.highlight })
+	set_highlight("Search", { bg = colors.mint_dark, fg = colors.fg })
+	set_highlight("IncSearch", { bg = colors.yellow, fg = colors.bg })
+
+	-- Treesitter
+	set_highlight("TSKeyword", { fg = colors.blue, bold = true })
+	set_highlight("TSFunction", { fg = colors.purple, bold = true })
+	set_highlight("TSVariable", { fg = colors.orange })
+	set_highlight("TSParameter", { fg = colors.mint })
+
+	-- StatusLine
+	set_highlight("StatusLine", { bg = colors.bg_alt, fg = colors.fg })
+	set_highlight("StatusLineNC", { bg = colors.bg, fg = colors.fg_dim })
 end
 
 return M
