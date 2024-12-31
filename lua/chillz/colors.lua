aa36bd6c3176e763b85f58f9f98ca3099b76a317
Maciej Gierałtowski <mgieraltowski02@gmail.com>
2024-12-31 19:08:31 +0100

    slightly color tweak and changed hilights gorups

--- lua/chillz/colors.lua
+++ lua/chillz/colors.lua
@@ -1,17 +1,20 @@
 local M = {}
 
 M.palette = {
-	bg = "#0b1724", -- bardzo ciemny granat
-	bg_alt = "#102a42", -- ciemny niebieski (sekundarne tło)
-	fg = "#d8eefe", -- jasny, lodowy błękit
-	fg_alt = "#b5d6f2", -- pastelowy, zimny błękit
-	accent = "#58c4dd", -- jasny błękit
-	highlight = "#aef3ff", -- śnieżnobiały błękit
-	error = "#e95678", -- różowo-czerwień
-	warning = "#ffd670", -- jasny żółty
-	info = "#6acaff", -- subtelny błękit
-	success = "#81e3a5", -- delikatna zieleń
-	gray = "#748a98", -- neutralny szary
-}
+ -- Główne kolory
+    bg = "#0b1724",         -- bardzo ciemny granat (tło)
+    bg_alt = "#102a42",     -- ciemny niebieski (sekundarne tło)
+    fg = "#d8eefe",         -- jasny, lodowy błękit
+    fg_alt = "#b5d6f2",     -- pastelowy błękit
+    main = "#98fbcb",       -- główny kolor (miętowy błękit)
+    accent = "#58c4dd",     -- jasny błękit
+    highlight = "#aef3ff",  -- kontrastowy lodowy błękit
+
+    -- Dodatkowe kolory
+    error = "#e95678",      -- różowo-czerwień
+    warning = "#ffd670",    -- jasny żółty
+    info = "#6acaff",       -- subtelny błękit
+    success = "#81e3a5",    -- pastelowa zieleń
+    gray = "#748a98",       -- neutralny szary}
 
 return M
--- lua/chillz/highlights.lua
+++ lua/chillz/highlights.lua
@@ -4,10 +4,17 @@ local colors = require("chillz.colors").palette
 M.setup = function()
 	return {
 		Normal = { fg = colors.fg, bg = colors.bg },
-		Comment = { fg = colors.accent, style = "italic" },
+		NormalFloat = { fg = colors.fg_alt, bg = colors.bg_alt },
+		Comment = { fg = colors.gray, style = "italic" },
+		Identifier = { fg = colors.main },
+		Statement = { fg = colors.main, style = "bold" },
+		Keyword = { fg = colors.accent },
+		Function = { fg = colors.main, style = "bold" },
+		String = { fg = colors.success },
 		Error = { fg = colors.error, style = "bold" },
 		Warning = { fg = colors.warning },
-		-- Dodaj więcej grup highlightów
+		Info = { fg = colors.info },
+		Highlight = { fg = colors.highlight, bg = colors.bg_alt },
 	}
 end
 
