8402ae8bfaee04b2d0ef0f936d45bec11b2048c0
Maciej Gierałtowski <mgieraltowski02@gmail.com>
2024-12-31 19:18:14 +0100

    .

--- lua/chillz/colors.lua
+++ lua/chillz/colors.lua
@@ -1,20 +1,20 @@
 local M = {}
 
 M.palette = {
- -- Główne kolory
-    bg = "#0b1724",         -- bardzo ciemny granat (tło)
-    bg_alt = "#102a42",     -- ciemny niebieski (sekundarne tło)
-    fg = "#d8eefe",         -- jasny, lodowy błękit
-    fg_alt = "#b5d6f2",     -- pastelowy błękit
-    main = "#98fbcb",       -- główny kolor (miętowy błękit)
-    accent = "#58c4dd",     -- jasny błękit
-    highlight = "#aef3ff",  -- kontrastowy lodowy błękit
+	-- Główne kolory
+	bg = "#0b1724", -- bardzo ciemny granat (tło)
+	bg_alt = "#102a42", -- ciemny niebieski (sekundarne tło)
+	fg = "#d8eefe", -- jasny, lodowy błękit
+	fg_alt = "#b5d6f2", -- pastelowy błękit
+	main = "#98fbcb", -- główny kolor (miętowy błękit)
+	accent = "#58c4dd", -- jasny błękit
+	highlight = "#aef3ff", -- kontrastowy lodowy błękit
 
-    -- Dodatkowe kolory
-    error = "#e95678",      -- różowo-czerwień
-    warning = "#ffd670",    -- jasny żółty
-    info = "#6acaff",       -- subtelny błękit
-    success = "#81e3a5",    -- pastelowa zieleń
-    gray = "#748a98",       -- neutralny szary}
-
+	-- Dodatkowe kolory
+	error = "#e95678", -- różowo-czerwień
+	warning = "#ffd670", -- jasny żółty
+	info = "#6acaff", -- subtelny błękit
+	success = "#81e3a5", -- pastelowa zieleń
+	gray = "#748a98", -- neutralny szary}
+}
 return M
