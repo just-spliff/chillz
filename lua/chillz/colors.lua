83340b3d9b76b04b0d5f459d5ae5a559057b6c0b
Maciej Gierałtowski <mgieraltowski02@gmail.com>
2025-01-01 17:27:58 +0100

    .

--- lua/chillz/colors.lua
+++ lua/chillz/colors.lua
@@ -1,41 +1,29 @@
 local M = {}
 
 M.palette = {
-	-- Tło
-	bg = "#162326", -- Tło główne (ciemny, mroźny)
-	bg_alt = "#1D3339", -- Alternatywne tło (lekko jaśniejsze)
-	bg_dim = "#0E1A1C", -- Przyciemnione tło
-	bg_highlight = "#245157", -- Tło podświetlenia (nasycone)
+	-- Backgrounds
+	bg = "#011423", -- Main background (dark, frosty)
+	bg_alt = "#122B3A", -- Alternative background (cooler and slightly lighter)
+	frost_shadow = "#082028", -- Dimmed background (muted)
+	highlight_glow = "#244B56", -- Highlight background (icy)
 
-	-- Tekst
-	fg = "#E4FFFF", -- Główny kolor tekstu (bardzo jasny, chłodny)
-	fg_alt = "#AEDFE5", -- Alternatywny kolor tekstu (miętowy)
-	fg_dim = "#5F8A8E", -- Przyciemniony tekst (spokojny miętowy)
-	fg_light = "#F0FFFF", -- Bardzo jasny tekst (prawie biały)
+	-- Text
+	fg = "#D9FFFF", -- Main text color (crystal white)
+	soft_mint = "#A4DBE2", -- Alternative text color (soft mint)
+	frosted_steel = "#5C9CA3", -- Dimmed text color (muted steel)
+	glacier_light = "#F0FFFF", -- Very light text (glacial white)
 
-	-- Akcenty
-	mint_blue = "#6FE7E8", -- Kolor przewodni (nasycony miętowy błękit)
-	mint_dark = "#49C4C7", -- Ciemniejszy miętowy
-	ice_blue = "#ACFFFF", -- Intensywny lodowy błękit
-	frost_green = "#3DDCD2", -- Zielony mroźny (jasny i chłodny)
-	cyan = "#3FB8C8", -- Intensywny cyan
+	-- Accents
+	polar_blue = "#6FE7E8", -- Main accent color (vivid polar blue)
+	bright_ice = "#47FF9C", -- Main accent color (bright frosty green)
+	aurora_sky = "#9EEFEF", -- Soft icy blue
+	seafoam_green = "#3DDCD2", -- Subtle frosty green
 
-	-- Kontrasty
-	warning = "#F2AF5F", -- Ostrzeżenie (żywy pomarańczowy kontrast)
-	error = "#F25C54", -- Błąd (mocny czerwony odcień)
-	success = "#5FF2A1", -- Sukces (jasna, nasycona zieleń)
-	info = "#3DAEEF", -- Informacja (żywy, jasny niebieski)
-
-	-- Specjalne
-	purple = "#A485F2", -- Fioletowy akcent (nasycony)
-	deep_blue = "#3B6FCC", -- Głęboki niebieski (żywy)
-	magenta = "#E08DFF", -- Magenta (intensywna)
-	pink = "#FFB8C5", -- Jasny róż (ciepły kontrast)
-	orange = "#F29367", -- Żywy pomarańczowy (lekko ciepły)
-
-	-- Gradienty
-	gradient_start = "#6FE7E8", -- Start gradientu (miętowy błękit)
-	gradient_mid = "#49C4C7", -- Środek gradientu (ciemniejszy miętowy)
-	gradient_end = "#3DDCD2", -- Koniec gradientu (zielonkawy)
+	-- Contrasts
+	icy_cyan = "#37BAC6", -- Deep icy cyan
+	frozen_purple = "#8B97E5", -- Cool frosty purple
+	winter_rose = "#CE9FFF", -- Winter rose (soft magenta)
+	chilled_pink = "#FFB8C5", -- Frosty pink (soft contrast)
 }
+
 return M
--- lua/chillz/highlights.lua
+++ lua/chillz/highlights.lua
@@ -6,38 +6,38 @@ M.setup = function(colors)
 	local bg_color = config.transparent and "NONE" or colors.bg
 
 	return {
-		-- Tło i tekst
+		-- Background and text
 		Normal = { fg = colors.fg, bg = bg_color },
-		Comment = { fg = colors.fg_dim, italic = true },
-		CursorLine = { bg = colors.bg_highlight },
-		CursorLineNr = { fg = colors.mint_blue, bold = true },
-		LineNr = { fg = colors.fg_dim },
+		Comment = { fg = colors.frosted_steel, italic = true },
+		CursorLine = { bg = colors.highlight_glow },
+		CursorLineNr = { fg = colors.polar_blue, bold = true },
+		LineNr = { fg = colors.frosted_steel },
 		Visual = { bg = colors.bg_alt },
 
-		-- Akcenty
-		Identifier = { fg = colors.mint_blue },
-		Function = { fg = colors.ice_blue, bold = true },
-		Keyword = { fg = colors.cyan, italic = true },
-		String = { fg = colors.success },
-		Constant = { fg = colors.frost_green },
-		Type = { fg = colors.deep_blue },
-		Special = { fg = colors.purple },
-		WarningMsg = { fg = colors.warning, bold = true },
-		ErrorMsg = { fg = colors.error, bold = true },
+		-- Accents
+		Identifier = { fg = colors.polar_blue },
+		Function = { fg = colors.aurora_sky, bold = true },
+		Keyword = { fg = colors.icy_cyan, italic = true },
+		String = { fg = colors.bright_ice },
+		Constant = { fg = colors.seafoam_green },
+		Type = { fg = colors.frozen_purple },
+		Special = { fg = colors.winter_rose },
+		WarningMsg = { fg = colors.chilled_pink, bold = true },
+		ErrorMsg = { fg = colors.frozen_purple, bold = true },
 
-		-- Statusline i inne
+		-- Statusline and other UI elements
 		StatusLine = { fg = colors.fg, bg = colors.bg_alt },
-		StatusLineNC = { fg = colors.fg_dim, bg = colors.bg_dim },
+		StatusLineNC = { fg = colors.frosted_steel, bg = colors.frost_shadow },
 		Pmenu = { fg = colors.fg, bg = colors.bg_alt },
-		PmenuSel = { fg = colors.bg, bg = colors.mint_blue },
-		TabLine = { fg = colors.fg_dim, bg = colors.bg_dim },
+		PmenuSel = { fg = colors.bg, bg = colors.polar_blue },
+		TabLine = { fg = colors.frosted_steel, bg = colors.frost_shadow },
 		TabLineSel = { fg = colors.fg, bg = colors.bg_alt },
 
-		-- Gradient
-		DiffAdd = { fg = colors.gradient_start },
-		DiffChange = { fg = colors.gradient_mid },
-		DiffDelete = { fg = colors.error },
-		DiffText = { fg = colors.gradient_end },
+		-- Diff highlighting (gradients)
+		DiffAdd = { fg = colors.seafoam_green },
+		DiffChange = { fg = colors.polar_blue },
+		DiffDelete = { fg = colors.chilled_pink },
+		DiffText = { fg = colors.aurora_sky },
 	}
 end
 
