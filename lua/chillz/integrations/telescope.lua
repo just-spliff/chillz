7a6a939cde444a0b960238f13fdadc25bc743d93
Maciej Gierałtowski <mgieraltowski02@gmail.com>
2025-01-01 16:27:34 +0100

    few subtle changes in telescope integration

--- lua/chillz/integrations/telescope.lua
+++ lua/chillz/integrations/telescope.lua
@@ -10,19 +10,19 @@ telescope.setup({
 		entry_prefix = "  ",
 		initial_mode = "insert",
 		sorting_strategy = "ascending",
-		layout_strategy = "horizontal", -- Użyj innego układu (horizontal)
+		layout_strategy = "horizontal", -- Układ poziomy
 		layout_config = {
 			prompt_position = "top", -- Pozycja paska wejściowego
 			width = 0.8, -- Szerokość okna
 		},
-		color_devicons = true,
+		winblend = 10, -- Przezroczystość okna
+		color_devicons = true, -- Kolory ikon
 		mappings = {
 			i = {
 				["<Esc>"] = actions.close,
 			},
 		},
 		path_display = { "smart" },
-		winblend = 10, -- Przezroczystość okna
 	},
 	pickers = {
 		find_files = {
@@ -47,3 +47,15 @@ telescope.setup({
 
 -- Włącz rozszerzenie fzf
 telescope.load_extension("fzf")
+
+-- Ustawienie kolorów dla Telescope
+vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = colors.bg, fg = colors.fg }) -- Tło i tekst
+vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = colors.bg_alt, fg = colors.fg }) -- Tło paska wejściowego
+vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = colors.bg_alt, fg = colors.bg_alt }) -- Obramowanie paska wejściowego
+vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = colors.bg_highlight, fg = colors.fg }) -- Tło podglądu
+vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = colors.bg_highlight, fg = colors.bg_highlight }) -- Obramowanie podglądu
+vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = colors.bg, fg = colors.fg }) -- Tło wyników
+vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = colors.bg, fg = colors.bg }) -- Obramowanie wyników
+vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = colors.mint_blue, fg = colors.bg }) -- Wyróżniony wynik
+vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", { fg = colors.success }) -- Wskaźnik na wyróżniony wynik
+vim.api.nvim_set_hl(0, "TelescopeMatching", { fg = colors.cyan, bold = true }) -- Dopasowane części tekstu
