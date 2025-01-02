86ad87ca6c84c3d35559ea6c96af9150caba7790
Maciej Gierałtowski <mgieraltowski02@gmail.com>
2025-01-02 13:17:56 +0100

    .

--- lua/chillz/groups/syntax.lua
+++ lua/chillz/groups/syntax.lua
@@ -1,60 +1,43 @@
 local M = {}
 
-function M.get()
-	return {
-		Comment = { fg = M.palette.comment, style = { "italic" } }, -- just comments
-		SpecialComment = { link = "Special" }, -- special things inside a comment
-		Constant = { fg = M.palette.ice_blue }, -- (preferred) any constant
-		String = { fg = M.palette.green, style = { "italic" } }, -- a string constant: "this is a string"
-		Character = { fg = M.palette.teal }, --  a character constant: 'c', '\n'
-		Number = { fg = M.palette.ice_blue, style = {} }, --   a number constant: 234, 0xff
-		Float = { link = "Number" }, --    a floating point constant: 2.3e10
-		Boolean = { fg = M.palette.green_dark, style = {} }, --  a boolean constant: TRUE, false
-		Identifier = { fg = M.palette.mint_blue_dark, style = {} }, -- (preferred) any variable name
-		Function = { fg = M.palette.blue, style = { "bold" } }, -- function name (also: methods for classes)
-		Statement = { fg = M.palette.purple }, -- (preferred) any statement
-		Conditional = { fg = M.palette.purple_dark, style = {} }, --  if, then, else, endif, switch, etc.
-		Repeat = { fg = M.palette.purple_dark, style = {} }, --   for, do, while, etc.
-		Label = { fg = M.palette.cyan }, --    case, default, etc.
-		Operator = { fg = M.palette.teal, style = {} }, -- "sizeof", "+", "*", etc.
-		Keyword = { fg = M.palette.magenta, style = { "bold" } }, --  any other keyword
-		Exception = { fg = M.palette.magenta_dark, style = {} }, --  try, catch, throw
+M.setup = function(colors)
+	local bg_color = colors.bg
+	local highlights = {
+		-- General UI
+		Normal = { fg = colors.fg, bg = bg_color },
+		Comment = { fg = colors.comment, italic = true },
+		CursorLine = { bg = colors.bg_highlight },
+		CursorLineNr = { fg = colors.mint_blue, bold = true },
+		LineNr = { fg = colors.fg_dim },
+		Visual = { bg = colors.bg_alt },
+		MatchParen = { bg = colors.bg_highlight, bold = true },
+		Pmenu = { fg = colors.fg, bg = colors.bg_alt },
+		PmenuSel = { fg = colors.bg, bg = colors.mint_blue_dark },
+		StatusLine = { fg = colors.fg, bg = colors.bg_alt },
+		StatusLineNC = { fg = colors.fg_dim, bg = colors.bg_dim },
+		TabLine = { fg = colors.fg_dim, bg = colors.bg_dim },
+		TabLineSel = { fg = colors.fg, bg = colors.bg_alt },
+		TabLineFill = { bg = colors.bg_dim },
 
-		PreProc = { fg = M.palette.mint_blue }, -- (preferred) generic Preprocessor
-		Include = { fg = M.palette.magenta_dark, style = {} }, --  preprocessor #include
-		Define = { link = "PreProc" }, -- preprocessor #define
-		Macro = { fg = M.palette.purple }, -- same as Define
-		PreCondit = { link = "PreProc" }, -- preprocessor #if, #else, #endif, etc.
-
-		StorageClass = { fg = M.palette.teal }, -- static, register, volatile, etc.
-		Structure = { fg = M.palette.teal }, --  struct, union, enum, etc.
-		Special = { fg = M.palette.mint_blue }, -- (preferred) any special symbol
-		Type = { fg = M.palette.yellow, style = { "italic" } }, -- (preferred) int, long, char, etc.
-		Typedef = { link = "Type" }, --  A typedef
-
-		-- UI elements
-		Error = { fg = M.palette.red_dark }, -- any erroneous construct
-		Todo = { bg = M.palette.teal, fg = M.palette.bg, style = { "bold" } }, -- TODO
-		qfLineNr = { fg = M.palette.yellow },
-		qfFileName = { fg = M.palette.blue },
-		htmlH1 = { fg = M.palette.mint_blue, style = { "bold" } },
-		htmlH2 = { fg = M.palette.green_dark, style = { "bold" } },
-
-		-- Debugging
-		debugPC = { bg = M.palette.bg_highlight }, -- used for highlighting the current line in terminal-debug
-		debugBreakpoint = { bg = M.palette.bg_dark, fg = M.palette.fg_gutter },
-
-		-- Illuminate
-		illuminatedWord = { bg = M.palette.bg_dim },
-		illuminatedCurWord = { bg = M.palette.bg_dim },
+		-- Diagnostics
+		DiagnosticError = { fg = colors.magenta },
+		DiagnosticWarn = { fg = colors.purple_dark },
+		DiagnosticInfo = { fg = colors.mint_blue_dark },
+		DiagnosticHint = { fg = colors.teal },
 
 		-- Diff
-		diffAdded = { fg = M.palette.green },
-		diffRemoved = { fg = M.palette.red_dark },
-		diffChanged = { fg = M.palette.mint_blue },
-		diffOldFile = { fg = M.palette.yellow },
-		diffNewFile = { fg = M.palette.ice_blue },
+		DiffAdd = { fg = colors.green, bg = colors.bg_dim },
+		DiffChange = { fg = colors.blue, bg = colors.bg_dim },
+		DiffDelete = { fg = colors.purple_dark, bg = colors.bg_dim },
+		DiffText = { fg = colors.blue_light, bg = colors.bg_highlight },
+
+		-- Git
+		GitSignsAdd = { fg = colors.green },
+		GitSignsChange = { fg = colors.blue },
+		GitSignsDelete = { fg = colors.purple_dark },
 	}
+
+	return highlights
 end
 
 return M
