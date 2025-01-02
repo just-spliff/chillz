3a30ae1fca85cbe97afdac362016f8383566e6e1
Maciej Gierałtowski <mgieraltowski02@gmail.com>
2025-01-02 14:47:29 +0100

    .

---
+++ lua/chillz/groups/integrations/alpha.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+M.setup = function(colors)
+	return {
+		-- Define highlight groups for alpha dashboard
+		AlphaShortcut = { fg = colors.green }, -- Bright green for shortcuts
+		AlphaHeader = { fg = colors.mint_blue }, -- Primary mint blue for headers
+		AlphaHeaderLabel = { fg = colors.teal }, -- Teal for header labels
+		AlphaButtons = { fg = colors.blue }, -- Bright blue for buttons
+		AlphaFooter = { fg = colors.cyan, style = { "italic" } }, -- Cyan for footer with italic style
+	}
+end
+
+return M
--- lua/chillz/init.lua
+++ lua/chillz/init.lua
@@ -1,15 +1,18 @@
-local config = require("chillz.config")
-local M = {}
-
 M.setup = function(opts)
 	opts = opts or {}
 
 	-- Wczytaj kolory i grupy highlight
 	local colors = require("chillz.colors").palette
 	local highlights = require("chillz.groups.syntax").setup(colors)
+	local alpha_integration = require("chillz.groups.integrations.alpha").setup(colors)
 
 	-- Zastosuj highlighty
 	for group, settings in pairs(highlights) do
+		vim.api.nvim_set_hl(0, group, settings)
+	end
+
+	-- Zastosuj integracje z alpha-nvim
+	for group, settings in pairs(alpha_integration) do
 		vim.api.nvim_set_hl(0, group, settings)
 	end
 
@@ -19,5 +22,3 @@ M.setup = function(opts)
 
 	config.apply_transparency()
 end
-
-return M
